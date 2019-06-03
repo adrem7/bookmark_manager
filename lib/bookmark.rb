1d124aaad87575523169242a1591355cfe1c118d
Anthony Hull <m@anthonyhull.me>
2019-06-03 17:01:16 +0100

    refactor to use views folder not view you idiot

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,28 +1,20 @@
 # frozen_string_literal: true
 
 require 'pg'
+require 'pry-byebug'
 class Bookmark
   def self.all
 
-
     begin
-        connection = PG.connect :dbname => 'bookmark_manager', :user => 'bookmark'
-        
-        response = connection.exec "SELECT * FROM bookmarks LIMIT 5"
-    
-        response.each do |row|
-          puts "%s %s" % [ row['id'], row['url']]
-        end
-        
+      connection = PG.connect :dbname => 'bookmark_manager', :user => 'bookmark'
+      
+      response = connection.exec "SELECT * FROM bookmarks LIMIT 5"
+      response.map { |bookmark| bookmark['url'] }
+
     rescue PG::Error => error
-    
-        puts error.message 
-        
-    ensure
-    
-        response.clear if response
-        connection.close if connection
-        
+      puts error.message 
+    ensure   
+      connection.close if connection
     end
   end
 end
